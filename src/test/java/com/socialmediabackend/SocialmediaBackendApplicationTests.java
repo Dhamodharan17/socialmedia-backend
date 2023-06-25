4ecb64b0c6049c93ddd4c2149d8f2820ab47b382
Karthik <msdhamodharan5@gmail.com>
2023-06-25 16:01:57 +0530

    initial commit

---
+++ src/test/java/com/socialmediabackend/SocialmediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
